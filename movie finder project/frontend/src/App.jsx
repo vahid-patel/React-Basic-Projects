bd0e602baba94c2b3c9d9686928b8f4677531b58
vahid-patel <144371170+vahid-patel@users.noreply.github.com>
2025-03-28 00:05:02 +0530

    changes

--- movie finder project/frontend/src/App.jsx
+++ movie finder project/frontend/src/App.jsx
@@ -2,15 +2,10 @@ import MovieCard from './components/MovieCard'
 import './App.css'
 
 function App() {
-  const movienumber = 1;
 
   return (
     <>
-    {movienumber === 1 ? (     
-       <MovieCard movie={{title:"Krish 4",release_date:"2025"}}/>
-):(      
-<MovieCard movie={{title:"War 2",release_date:"2025"}}/>
-)}
+   
     </>
   )
 }
