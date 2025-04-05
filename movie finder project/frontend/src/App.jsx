409092823edbef595a8f96e804a85c90abd0be34
vahid-patel <144371170+vahid-patel@users.noreply.github.com>
2025-04-05 18:52:40 +0530

    changes

--- movie finder project/frontend/src/App.jsx
+++ movie finder project/frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import MovieCard from './components/MovieCard'
 import './App.css'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-   
+      <Home />
     </>
   )
 }
---
+++ movie finder project/frontend/src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import MovieCard from "../components/MovieCard";
+
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const movie = [
+    { id: 1, title: "Kick", release_date: "2014" },
+    { id: 2, title: "Bahubali", release_date: "2016" },
+    { id: 3, title: "dangal", release_date: "2016" },
+    { id: 4, title: "jawan", release_date: "2024" },
+  ];
+  const handleSearch = (e) => {
+      e.preventDefault()
+      alert("clicked search")
+      setSearchQuery("bye")
+  };
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for Movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        ></input>
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div className="movies-grid">
+        {movie.map((movie) => (
+          <MovieCard movie={movie} key={movie.id}></MovieCard>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
